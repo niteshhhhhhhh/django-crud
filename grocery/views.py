8d08c978fd513c8adf85f1bf4a15d07f43674f29
Nitesh <np3234@gmail.com>
2026-02-22 20:04:40 +0545

    Dummy data using shell

--- grocery/views.py
+++ grocery/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import GroceryItem
 
-# Create your views here.
+
+def index(request):
+    """Display all grocery items"""
+    items = GroceryItem.objects.all()
+    context = {
+        'items': items,
+    }
+    return render(request, 'grocery/index.html', context)
